f6cee7bf371406587792668c060ecc039a0cb63c
Paul Dumoulin <pdumoulin@theorchard.com>
2020-08-07 09:56:53 -0400

    handle muli-line inputs, just in case

--- scripts/b64json.py
+++ scripts/b64json.py
@@ -5,10 +5,15 @@
 import base64
 import json
 import pprint
+import traceback
 import sys
 
 for line in [x for x in sys.stdin]:
     print(line)
     print('--------')
-    output = json.loads(base64.b64decode(line).decode('utf-8'))
-    pprint.pprint(output)
+    try:
+        output = json.loads(base64.b64decode(line).decode('utf-8'))
+        pprint.pprint(output)
+    except Exception:
+        traceback.print_exc()
+    print('')
