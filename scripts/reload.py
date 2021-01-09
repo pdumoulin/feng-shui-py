54617166f1258fe3f641d0d50a969b48b6141ec0
Paul Dumoulin <pdumoulin@theorchard.com>
2021-01-08 20:21:00 -0500

    switch to using regex

--- scripts/reload.py
+++ scripts/reload.py
@@ -2,24 +2,26 @@
 """Perform action when saving file matching pattern in dir."""
 
 import os
+import re
 import sys
 import time
 from subprocess import call
 
-# key: directory to watch, inclusing sub dirs
-# value:
-#   extensions    : list of file extensions to monitor for changes (default: *)
-#   action        : command to run if matched (default: echo noop)
-#   sub_dir_level : num dirs to move up before running command (default 0)
+# key: regex to match file path
+#   action: command to perform on match
+#   dir_index: zero-ed index of dir to run command in
 CONFIG = {
-    '~/projects/collab/pdumoulin/lambda-ecr-poc': {
-        'extensions': ['py'],
-        'action': 'docker-compose restart fibcow || docker-compose up -d fibcow'  # noqa:E501
+    '~/projects/collab/pdumoulin/lambda-ecr-poc/.*py$': {
+        'action': 'docker-compose restart fibcow || docker-compose up -d --build fibcow',  # noqa:E501
+        'dir_index': 3
     },
-    '~/projects/': {
-        'extensions': ['py'],
+    '~/projects/collab/pdumoulin/lambda-ecr-poc/.*\\.(yml|yaml)$': {
+        'action': 'docker-compose up -d --build fibcow',
+        'dir_index': 3
+    },
+    '~/projects/.*\\.py$': {
         'action': 'touch -c app.py',
-        'sub_dir_level': 1
+        'dir_index': 1
     }
 }
 
@@ -31,21 +33,24 @@ def main():
     # full file path being saved
     file_path = sys.argv[1]
 
-    # load config, expand directory paths
-    config = load_config()
+    # expand homedir and add defaults
+    config = load_config(CONFIG)
 
-    # only include configs matching file location and extension
-    filtered_config = filter_config(file_path, config)
+    # filter out non-matching rules
+    config = filter_config(file_path, config)
+
+    # dir & action tupe of tasks to run
+    tasks = generate_tasks(file_path, config)
 
     # run commands in dirs according to config
-    for run_dir, action in generate_tasks(file_path, filtered_config):
+    for run_dir, action in tasks:
         cmd = f'cd {run_dir} && {action} ; cd --'
         print(f'$ {cmd}')
         call(cmd, shell=True)
 
     # output on runtime and configs matched
     print(f'Took {time.time() - start_time} seconds')
-    print(f'Matched {len(filtered_config.keys())} config(s)')
+    print(f'Matched {len(config.keys())} config(s)')
 
 
 def run_path(file_path, dir_path, num_levels):
@@ -57,45 +62,41 @@ def run_path(file_path, dir_path, num_levels):
     return os.path.join(dir_path, *sub_dir_parts)
 
 
+def sub_path(file_path, index):
+    """Truncate file path based on number of dirs."""
+    split_path = file_path.split(os.path.sep)[:index + 2]
+    return os.path.join(os.path.sep, *split_path)
+
+
 def generate_tasks(file_path, config):
     """Parse config to generate commands and dirs to run them in."""
     return [
         (
-            run_path(file_path, directory, setup.get('sub_dir_level')),
+            sub_path(file_path, setup['dir_index']),
             setup['action']
         )
-        for directory, setup in config.items()
+        for _, setup in config.items()
     ]
 
 
-def file_in_dir(file_path, dir_path):
-    """File is in dir according to path."""
-    return file_path.startswith(dir_path)
-
-
-def extension_matches(file_path, extensions):
-    """Filepath matches allow list of extensions."""
-    return extensions == '*' or any([file_path.endswith(f'.{x}') for x in extensions])  # noqa:E501
-
-
 def filter_config(file_path, config):
-    """Match config blocks relevant to file path."""
+    """Filter out non-matching dir patterns."""
     return {
         k: v for k, v in config.items()
-        if file_in_dir(file_path, k)
-        and extension_matches(file_path, v['extensions'])
+        if re.match(k, file_path)
     }
 
 
-def load_config():
-    """Normailize dir paths and fill in default configs."""
-    return {
-        os.path.expanduser(os.path.normpath(k)): {
-            'extensions': v.get('extensions', '*'),
-            'action': v.get('action', 'echo noop'),
-            'sub_dir_level': v.get('sub_dir_level', 0)
-        } for k, v in CONFIG.items()
-    }
+def load_config(config):
+    """Expand dir pattern and take into account homedir expansion for index."""
+    result = {}
+    for k, v in config.items():
+        norm_path = os.path.normpath(k)
+        ex_path = os.path.expanduser(norm_path)
+        path_diff = ex_path.count(os.path.sep) - norm_path.count(os.path.sep)
+        v['dir_index'] += path_diff
+        result[ex_path] = v
+    return result
 
 
 if __name__ == '__main__':
