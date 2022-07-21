8f28768bb4b3e36af6e69ac797a3169bd07d1587
Paul Dumoulin <pdumoulin@theorchard.com>
2022-07-21 16:36:42 -0400

    remove carriage returns as well

--- scripts/quote.py
+++ scripts/quote.py
@@ -3,5 +3,5 @@
 """Add double quotes to list of values from stdin."""
 
 import sys
-formatted = ["\n'%s'" % x.replace('\n', '') for x in sys.stdin]
+formatted = ["\n'%s'" % x.rstrip('\n\r')for x in sys.stdin]
 print(','.join(formatted))
